03ad843876fca3f6f745ee3c18562ab443953975
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2020-04-16 18:58:09 +0200

    Update selectionsort to use 'descending'-flag instead of own function

--- basic_sorting/sorts/selectionsort.py
+++ basic_sorting/sorts/selectionsort.py
@@ -1,4 +1,4 @@
-def selectionsort(list):
+def selectionsort(list, descending=False):
     """
     Takes in a list as an argument, and sorts it using a "Selection Sort"-algorithm.
 
@@ -10,38 +10,26 @@ def selectionsort(list):
     """
     length = len(list)
 
-    for i in range(length):
-        min_element_index = i
+    if descending:
+        for i in range(length):
+            max_element_index = i
 
-        for j in range (i+1, length):
-            if list[j] < list[min_element_index]:
-                min_element_index = j
-        
-        if min_element_index != i:
-            list[i], list[min_element_index] = list[min_element_index], list[i]
+            for j in range (i+1, length):
+                if list[max_element_index] < list[j]:
+                    max_element_index = j
+            
+            if max_element_index != i:
+                list[i], list[max_element_index] = list[max_element_index], list[i]
 
-    return list
+    else:   # Not descending, so must be ascending (low to high)
+        for i in range(length):
+            min_element_index = i
 
-def selectionsort_reversed(list):
-    """
-    Takes in a list as an argument, and sorts it using a "Selection Sort"-algorithm.
-
-    The list can be any length and contain any number of elements, as long as they have a 
-    defined "__gt__" (>)-operator.
-
-    Selection sort has a time complexity of O(n^2), but a maximum of n-1 swaps makes it viable if
-    swapping is expensive. For more information, check out wikipedia: https://en.wikipedia.org/wiki/Selection_sort
-    """
-    length = len(list)
-
-    for i in range(length):
-        max_element_index = i
-
-        for j in range (i+1, length):
-            if list[j] > list[max_element_index]:
-                max_element_index = j
-        
-        if max_element_index != i:
-            list[i], list[max_element_index] = list[max_element_index], list[i]
+            for j in range (i+1, length):
+                if list[j] < list[min_element_index]:
+                    min_element_index = j
+            
+            if min_element_index != i:
+                list[i], list[min_element_index] = list[min_element_index], list[i]
 
     return list
--- basic_sorting/tests/test_selectionsort.py
+++ basic_sorting/tests/test_selectionsort.py
@@ -38,41 +38,36 @@ class TestSelectionsort(unittest.TestCase):
         list_copy = list[:]
         self.assertEqual(selectionsort(list), sorted(list_copy))
 
+    def test_selectionsort_on_integers_descending(self):
+        list = _create_list_of_integers(25)
+        list_copy = list[:]
+        self.assertEqual(selectionsort(list, descending=True), sorted(list_copy, reverse=True))
+
     def test_selectionsort_on_floats(self):
         list = _create_list_of_floats(25)
         list_copy = list[:]
         self.assertEqual(selectionsort(list), sorted(list_copy))
+
+    def test_selectionsort_on_floats_descending(self):
+        list = _create_list_of_floats(25)
+        list_copy = list[:]
+        self.assertEqual(selectionsort(list, descending=True), sorted(list_copy, reverse=True))
 
     def test_selectionsort_on_letters(self):
         list = _create_list_of_letters(25)
         list_copy = list[:]
         self.assertEqual(selectionsort(list), sorted(list_copy))
 
+    def test_selectionsort_on_letters_descending(self):
+        list = _create_list_of_letters(25)
+        list_copy = list[:]
+        self.assertEqual(selectionsort(list, descending=True), sorted(list_copy, reverse=True))
+
     def test_selectionsort_error(self):
         list = [1, 4, 5.77, 2, "a"]
         self.assertRaises(TypeError, selectionsort, list)
 
 
-
-class TestSelectionsort_reversed(unittest.TestCase):
-    def test_selectionsort_reversed_on_integers(self):
-        list = _create_list_of_integers(25)
-        list_copy = list[:]
-        self.assertEqual(selectionsort_reversed(list), sorted(list_copy, reverse=True))
-
-    def test_selectionsort_reversed_on_floats(self):
-        list = _create_list_of_floats(25)
-        list_copy = list[:]
-        self.assertEqual(selectionsort_reversed(list), sorted(list_copy, reverse=True))
-
-    def test_selectionsort_reversed_on_letters(self):
-        list = _create_list_of_letters(25)
-        list_copy = list[:]
-        self.assertEqual(selectionsort_reversed(list), sorted(list_copy, reverse=True))
-
-    def test_selectionsort_reversed_error(self):
-        list = [1, 4, 5.77, 2, "a"]
-        self.assertRaises(TypeError, selectionsort_reversed, list)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
