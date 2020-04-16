07b1615669485919123dfb64e092f01e0921f47e
Andreas Berg <51373685+BollaBerg@users.noreply.github.com>
2020-04-16 16:20:43 +0200

    Add proper unit testing of selectionsort

--- test_selectionsort.py
+++ test_selectionsort.py
@@ -1,21 +1,52 @@
-import random
 from selectionsort import selectionsort
+import random
+import string
+import unittest
 
-def _create_list(n):
+def _create_list_of_integers(n):
+    """Create a list of n integers."""
     list = []
     for _ in range(n):
         list.append(random.randint(0, 100))
     
     return list
 
+def _create_list_of_floats(n):
+    """Create a list of n floats."""
+    list = []
+    for _ in range(n):
+        list.append(random.random() * 100)
+    
+    return list
 
-def test_selectionsort():
-    list = _create_list(25)
-    list_copy = list[:]
+def _create_list_of_letters(n):
+    """Create a list of n letters."""
+    list = []
+    for _ in range(n):
+        random_letter = random.choice(string.ascii_letters)
+        list.append(random_letter)
+    
+    return list
 
-    print(F"Unsorted list:\t\t {list}")
-    print(F"Own sorted list:\t {selectionsort(list)}")
-    print(F"Python-sorted list:\t {sorted(list_copy)}")
+class TestSelectionsort(unittest.TestCase):
+    def test_selectionsort_on_integers(self):
+        list = _create_list_of_integers(25)
+        list_copy = list[:]
+        self.assertEqual(selectionsort(list), sorted(list_copy))
 
-if __name__ == '__main__':
-    test_selectionsort()
+    def test_selectionsort_on_floats(self):
+        list = _create_list_of_floats(25)
+        list_copy = list[:]
+        self.assertEqual(selectionsort(list), sorted(list_copy))
+
+    def test_selectionsort_on_letters(self):
+        list = _create_list_of_letters(25)
+        list_copy = list[:]
+        self.assertEqual(selectionsort(list), sorted(list_copy))
+
+    def test_selectionsort_error(self):
+        list = [1, 4, 5.77, 2, "a"]
+        self.assertRaises(TypeError, selectionsort, list)
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
